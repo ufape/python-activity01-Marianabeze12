65dff0d8ff1a5a75dea734291f8be7e1fb7e2765
Marianabeze12 <125399980+Marianabeze12@users.noreply.github.com>
2023-02-23 15:10:03 -0300

    Add files via upload

---
+++ problem_4.py
@@ -0,0 +1,41 @@
+# -- coding: utf-8 --
+
+# Mariana
+# UAG00098
+# Problem Set 2 - Problem 4
+# Description:
+
+"""
+Inputs, Processes and Output (IPO)
+-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+Input(s):
+Digite 6 valores inteiros quaisquer.
+Exemplo:
+Digite o valor 1/6: 2
+Digite o valor 2/6: 3
+Digite o valor 3/6: 4
+Digite o valor 4/6: 5
+Digite o valor 5/6: 6
+Digite o valor 6/6: 7
+
+Processes:
+Faça um programa que leia 6 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
+
+Output(s):
+Exemplo:
+Você digitou 3 valores pares.
+"""
+
+
+def main():
+    num_pares = 0
+
+    for i in range(6):
+      valor = int(input(f"Digite o valor {i+1}/6:"))
+      if valor % 2 == 0:
+       num_pares += 1
+      print(f"Você digitou {num_pares} valores pares.")
+
+
+if _name_ == '_main_':
+    main()
